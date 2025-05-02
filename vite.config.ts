ebc5268cb7579083bf9c92573e1e76b6aa243e49
victor-estevam <106971816+cruyff21@users.noreply.github.com>
2025-05-02 20:01:49 -0300

    ajuste no vitte

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path'; // precisa importar o 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/dev-portfolio/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // define que '@' aponta para a pasta src
+    },
+  },
 });
